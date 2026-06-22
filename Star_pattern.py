50dcb6b0f7f16062ae14c13e4aa106d50edd1136
Ganesh Mali <ganeshmali222001@gmail.com>
2026-06-22 14:57:48 +0530

    update the star pattern file

---
+++ Star_pattern.py
@@ -0,0 +1,9 @@
+n = 5
+for i in range(1, n+1):      # rows 1 to n
+    for j in range(i):          # print i stars per row
+        print("*", end=" ")
+    print()                     # newline after each row
+
+
+
+
